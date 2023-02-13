ae05a4a6f2d37690814c3d9cb4f2a0b16e917500
Maxence <maxence.allart@gmail.com>
2023-02-13 12:34:02 +0100

    minidashboard finished

--- src/components/TestComponent.jsx
+++ src/components/TestComponent.jsx
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import PincodeInput from "@laurent-debug/react-pincode-input/dist/PincodeInput";
+
+
+
 
 function TestComponent() {
   const [count, setCount] = useState(0);
+  
 
   // Équivalent à componentDidMount plus componentDidUpdate :
   useEffect(() => {
     // Mettre à jour le titre du document en utilisant l'API du navigateur
     document.title = `Vous avez cliqué ${count} fois`;
   });
+
+  const onCompleted = (pincode) => {
+    console.log(pincode);
+  };
 
   return (
     <div>
@@ -16,6 +25,9 @@ function TestComponent() {
       <button onClick={() => setCount(count + 1)}>
         Cliquez ici
       </button>
+
+      <h5 className="text-center pt-2">Saisir votre code secret</h5>
+      <PincodeInput  onCompleted={onCompleted}  digitsNumber={4}/>
     </div>
   );
 }
--- src/components/TodoEditor/TodoEditor.jsx
+++ src/components/TodoEditor/TodoEditor.jsx
@@ -1,17 +1,132 @@
-
+import { useEffect , useState , useRef } from "react";
 
 function TodoEditor(props) {
 
     const {todos} = props;
 
+    const TodoEditorForm = useRef();
 
+    const nomTodoRef = useRef();
+    const descTodoRef = useRef();
+    const idTodoRef = useRef();
+    const todoFormSelector = useRef();
+
+    const add_todo_submitRef= useRef();
+    const modif_todo_submitRef= useRef();
+    const supp_todo_submitRef= useRef();
+    const del_todo_submitRef= useRef();
+
+
+    useEffect( ()=> {
+
+    }, [])
+
+
+    const handleSelectChange = (event) => {
+        const selectedId = event.target.value;
+        const selectedTodo = todos.find((todo) => todo.id == selectedId);
+        idTodoRef.current.value = selectedTodo ? selectedTodo.id : "";
+        nomTodoRef.current.value = selectedTodo ? selectedTodo.title : "";
+        descTodoRef.current.value = selectedTodo ? selectedTodo.description : "";
+      };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+        if (event.target.id === "add_todo_submit") 
+        {
+            console.log(event.target.id); 
+            const title = nomTodoRef.current.value;
+            const description = descTodoRef.current.value;
+            let body = {
+                title,
+                description
+              };
+              const myJSON = JSON.stringify(body);
+              console.log("json:", myJSON);
+              fetch("http://localhost:1337/todo", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: myJSON,
+              })
+                .then((response) => response.json())
+                .then((data) => console.log(data))
+                .catch((error) => console.error(error));
+        } else
+        if (event.target.id === "modif_todo_submit") 
+        {
+            console.log(event.target.id); 
+            const id = idTodoRef.current.value;
+            const title = nomTodoRef.current.value;
+            const description = descTodoRef.current.value;
+            let body = {
+                id,
+                title,
+                description
+              };
+              const myJSON = JSON.stringify(body);
+              console.log("json:", myJSON);
+              fetch("http://localhost:1337/todo/" + id, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: myJSON,
+              })
+                .then((response) => response.json())
+                .then((data) => console.log(data))
+                .catch((error) => console.error(error));            
+        } else 
+        if (event.target.id === "supp_todo_submit") 
+        {
+            console.log(event.target.id); 
+            const id = idTodoRef.current.value;
+            let body = {
+                id
+              };
+              const myJSON = JSON.stringify(body);
+              console.log("json:", myJSON);
+              fetch("http://localhost:1337/todo/" + id, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: myJSON,
+              })
+                .then((response) => response.json())
+                .then((data) => console.log(data))
+                .catch((error) => console.error(error));                  
+        } else 
+        if (event.target.id === "del_todo_submit") 
+        {
+            console.log(event.target.id); 
+            const id = idTodoRef.current.value;
+            let body = {
+                id
+              };
+              const myJSON = JSON.stringify(body);
+              console.log("json:", myJSON);
+              fetch("http://localhost:1337/todo/" + id, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: myJSON,
+              })
+                .then((response) => response.json())
+                .then((data) => console.log(data))
+                .catch((error) => console.error(error));   
+        }
+    };
 
 
     return (
 
     <> {console.log(todos)}
         
-            <div>
+            <form id="TodoEditorForm" ref={TodoEditorForm}>
 
                     <div className="container m-5">
                         <div className="mb-5">
@@ -21,9 +136,9 @@ function TodoEditor(props) {
 
                     <div className="input-group mb-3 mt-3">
                         <label className="input-group-text w-50" htmlFor="input-todo-select">Todos disponibles :</label>
-                            <select className="form-select" id="input-todo-select" name="todoFormSelect">
+                            <select className="form-select" id="input-todo-select" name="todoFormSelect" ref={todoFormSelector} onChange={handleSelectChange}>
                                 <option defaultValue>Ajouter...</option>
-                                {todos.map((todo) => <option key={"todo.id'+'todo.title"} value={todo.id}>{todo.title}</option>)}
+                                {todos.map((todo) => <option key={todo.id} value={todo.id}>{todo.title} </option>)}
                             </select>
                     </div>
 
@@ -31,18 +146,33 @@ function TodoEditor(props) {
                         <label className="form-label">Contenu de la todo ! :</label>
                             <div className="input-group">
                                 <span className="input-group-text w-50">Titre de la todo :</span>
-                                    <textarea id="titleTodo" className="form-control" placeholder="Saisir le nom de la todo."></textarea>
+                                    <textarea ref={nomTodoRef} id="nomTodo" className="form-control" placeholder="Saisir le nom de la todo."></textarea>
                             </div>
                     </div>
 
                     <div className="mb-3">
                         <div className="input-group">
                             <span className="input-group-text w-50">Description :</span>
-                            <textarea id="descTodo" className="form-control"  placeholder="Saisir la description de la todo."></textarea>
+                            <textarea ref={descTodoRef} id="descTodo" className="form-control"  placeholder="Saisir la description de la todo."></textarea>
                         </div>
-                    </div>        
+                    </div>       
 
-            </div>
+                    <div className="mb-3">
+                        <div className="input-group">
+                            <span className="input-group-text w-25">Id(readonly) :</span>
+                            <textarea ref={idTodoRef} id="idTodo" className="form-control text-bg-dark" readOnly placeholder="id here"></textarea>
+                        </div>
+                    </div> 
+
+                    <hr/>
+
+                    <button type="submit" className="btn btn-success m-1"   onClick={handleSubmit}  id="add_todo_submit" ref={add_todo_submitRef}>Ajouter</button>
+                    <button type="submit" className="btn btn-secondary m-1" onClick={handleSubmit}  id="modif_todo_submit" ref={modif_todo_submitRef}>Modifier</button>
+                    <button type="submit" className="btn btn-warning m-1"   onClick={handleSubmit}  id="supp_todo_submit" ref={supp_todo_submitRef}>Supprimer</button>
+                    <button type="submit" className="btn btn-danger m-1"    onClick={handleSubmit}  id="del_todo_submit" ref={del_todo_submitRef}>Effacer pour toujours !</button>
+
+            </form>
+            <hr />
     </>);
 }
 
